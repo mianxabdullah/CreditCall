5fa4403949e41b0321e2af1e90f4603ebe07cfb9
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-07-31 14:02:06 +0500

    Added home and /health endpoints

--- app/app.py
+++ app/app.py
@@ -6,3 +6,17 @@ app = FastAPI(title="Loan Approval API")
 model = joblib.load('models/loan_model.pkl')
 scaler = joblib.load('models/scaler.pkl')
 model_columns = joblib.load('models/model_columns.pkl')
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Loan Approval API!",
+            "status": "Active",
+            "version": "1.0.0",
+            "endpoints": "/health, /predict, /predict_file, /applicants, /applicants/{id} (GET/PUT/DELETE), /predictions, /predictions/{applicant_id}"
+            }
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "Healthy",
+            "version": "1.0.0",}
