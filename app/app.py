94f8940c302cee9e94f59608de94e6a76a7676bb
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-08-01 12:40:20 +0500

    added /predict endpoint to handle loan application predictions with authentication

--- app/app.py
+++ app/app.py
@@ -34,4 +34,40 @@ def health_check():
     return {"status": "Healthy",
             "version": "1.0.0",}
 
+@app.post("/predict", dependencies=[Depends(verify_api_key)])
+def predict(application: LoanApplication):
+    try:
+        input_dict = application.model_dump()
+        df = pd.DataFrame([input_dict])
+
+        scaled_input, _ = preprocess(df, scaler, model_columns)
+
+        prediction = model.predict(scaled_input)[0]
+        probability = model.predict_proba(scaled_input)[0][1]
+
+        applicant_id = save_applicant({
+            "gender": application.Gender,
+            "married": application.Married,
+            "dependents": application.Dependents,
+            "education": application.Education,
+            "self_employed": application.Self_Employed,
+            "applicant_income": application.ApplicantIncome,
+            "coapplicant_income": application.CoapplicantIncome,
+            "loan_amount": application.LoanAmount,
+            "loan_amount_term": application.Loan_Amount_Term,
+            "credit_history": application.Credit_History,
+            "property_area": application.Property_Area,
+        })
+
+        loan_status = "Approved" if prediction == 1 else "Rejected"
+        save_prediction(applicant_id, loan_status, round(float(probability), 4))
+
+        return {
+            "loan_status": loan_status,
+            "approval_probability": round(float(probability), 4)
+        }
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Prediction failed: {str(e)}")
+
 
